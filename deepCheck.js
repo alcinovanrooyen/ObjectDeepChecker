e13eea2687006cd608a78868fba9851ffc51eead
Alcino Van Rooyen <alcinovanrooyen@users.noreply.github.com>
2023-10-16 13:51:23 +0200

    Create deepCheck.js

---
+++ deepCheck.js
@@ -0,0 +1,30 @@
+/**
+ * An asyncronous and recursive way of checking that one object has all the properties that exsit on 
+ * another object.
+ * @params {Object} a - Object that needs to be checked.
+ * @params {Object} b - Object that b should be compared to / checked against.
+ */
+function deepCheck(a, b) {
+    return new Promise(async res => {
+        const aKeys   = Object.keys(a);
+        const bKeys   = Object.keys(b);
+        
+        for (const propA of aKeys) {
+            if (typeof b[propA] === 'object') {
+                if (!a[propA]) a[propA] = {};
+                await deepCheck(a[propA], b[propA]);
+            }
+            
+            else if (aKeys.indexOf(propA) === -1) {
+                let response = await new Promise( res => res()  ); // Any value / function that needs waiting for here.
+                a[propA]     = response || b[propA];
+            }
+        }
+        res();
+    });
+}
+
+
+// Usage: 
+// await deepCheck({}, {});
+
